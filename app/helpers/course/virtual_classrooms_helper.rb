693afa411d668099bb314590aa388533c43f1bdb
Yecheng Xu <xyc0562@gmail.com>
2017-03-06 18:26:06 +0800

    Use duration instead of end_at for virtual classroom; Require BrainCert API key for virtual classroom listing

--- app/helpers/course/virtual_classrooms_helper.rb
+++ app/helpers/course/virtual_classrooms_helper.rb
@@ -7,4 +7,8 @@ module Course::VirtualClassroomsHelper
   def virtual_classrooms_title
     @settings.title.nil? ? nil : format_inline_text(@settings.title)
   end
+
+  def duration_options
+    (15..(@settings.max_duration.to_i).to_i).step(15).map { |i| ["#{i} minutes", i] }
+  end
 end
